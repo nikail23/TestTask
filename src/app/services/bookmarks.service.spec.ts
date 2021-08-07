ef8d499e51fb11a795f095a81f426d4fd5a23bd4
Илья Ермолович <nikail232323232323@gmail.com>
2021-08-07 18:09:14 +0300

    Add bookmarks service tests

--- src/app/services/bookmarks.service.spec.ts
+++ src/app/services/bookmarks.service.spec.ts
@@ -1,6 +1,8 @@
+import { Bookmark } from './../main/bookmarks/bookmark';
 import { TestBed } from '@angular/core/testing';
 
 import { BookmarksService } from './bookmarks.service';
+import { Image } from '../main/search/image';
 
 describe('BookmarksService', () => {
   let service: BookmarksService;
@@ -12,5 +14,73 @@ describe('BookmarksService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should add bookmark correctly', () => {
+    const bookmark: Bookmark = {
+      image: {
+        url: 'test',
+        title: 'test'
+      },
+      tags: 'test'
+    };
+    service.addBookmark(bookmark);
+    expect(service.getBookmarks()[0]).toEqual(bookmark);
+  });
+
+  it('should save correctly', () => {
+    const bookmark: Bookmark = {
+      image: {
+        url: 'test',
+        title: 'test'
+      },
+      tags: 'test'
+    };
+    service.addBookmark(bookmark);
+    service.saveBookmarks();
+    const localStorageBookmarks = JSON.parse(localStorage.getItem('bookmarks') || '{}');
+
+    expect(service.getBookmarks()).toEqual(localStorageBookmarks);
+  })
+
+  it('should load correctly', () => {
+    const bookmark: Bookmark = {
+      image: {
+        url: 'test',
+        title: 'test'
+      },
+      tags: 'test'
+    };
+    service.addBookmark(bookmark);
+    service.saveBookmarks();
+    service.loadBookmarks();
+    const localStorageBookmarks = JSON.parse(localStorage.getItem('bookmarks') || '{}');
+
+    expect(service.getBookmarks()).toEqual(localStorageBookmarks);
+  })
+
+  it('should delete correctly', () => {
+    const bookmark: Bookmark = {
+      image: {
+        url: 'test',
+        title: 'test'
+      },
+      tags: 'test'
+    };
+    const bookmark2: Bookmark = {
+      image: {
+        url: 'test2',
+        title: 'test2'
+      },
+      tags: 'test2'
+    };
+    service.addBookmark(bookmark);
+    service.addBookmark(bookmark2);
+    service.saveBookmarks();
+    service.loadBookmarks();
+    service.deleteBookmark(0);
+    service.saveBookmarks();
+    service.loadBookmarks();
+    expect(service.getBookmarks()[1]).toBeUndefined();
   });
 });
