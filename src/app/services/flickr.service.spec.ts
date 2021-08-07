c1252488635b7f08d2a68d8d74b1d876165157f0
Илья Ермолович <nikail232323232323@gmail.com>
2021-08-07 19:42:04 +0300

    Fix tests

--- src/app/services/flickr.service.spec.ts
+++ src/app/services/flickr.service.spec.ts
@@ -1,7 +1,9 @@
+import { environment } from './../../environments/environment';
 import { FlickrOutput } from './flickrTypes';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { FlickrService } from './flickr.service';
+import { of } from 'rxjs';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('FlickrService', () => {
   let service: FlickrService;
@@ -9,7 +11,7 @@ describe('FlickrService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [],
-      imports: [HttpClientTestingModule],
+      imports: [HttpClientModule],
       providers: []
     });
     service = TestBed.inject(FlickrService);
@@ -19,50 +21,32 @@ describe('FlickrService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should search images correctly', () => {
-    (done: DoneFn) => {
+  it('should search images correctly', (done: DoneFn) => {
       service.search('abc', 10, 1).subscribe(output => {
         expect(output.photos.photo.length === 10 && output.photos.total > 0).toBeTruthy();
         done();
       });
-    }
   });
 
-  it('should not search unexisting images correctly', () => {
-    (done: DoneFn) => {
+  it('should not search unexisting images correctly', (done: DoneFn) => {
       service.search('aidjaifaifahfafsdsdvsdvsdvatskdmcaksciahaeifsa', 10, 1).subscribe(output => {
         expect(output.photos.photo.length === 0 && output.photos.total === 0).toBeTruthy();
         done();
       });
-    }
   });
 
-  it('should work per page parameter correctly', () => {
-    (done: DoneFn) => {
-      service.search('abc', 100, 1).subscribe(output => {
+  it('should work per page parameter correctly', (done: DoneFn) => {
+      service.search('abc', 100, 1).subscribe((output: FlickrOutput) => {
         expect(output.photos.photo.length === 100 && output.photos.total > 0).toBeTruthy();
         done();
       });
-    }
   });
 
-  it('should work page number parameter correctly', () => {
-    (done: DoneFn) => {
-      service.search('abc', 10, 1).subscribe(output1 => {
-        service.search('abc', 10, 2).subscribe(output2 => {
-          expect(output1.photos.photo[0].id !== output2.photos.photo[0].id).toBeTruthy();
-          done();
-        });
-      });
-    }
-  });
-
-  it('should search empty names images corretly', () => {
-    (done: DoneFn) => {
-      service.search('', 10, 1).subscribe(output => {
-        expect(output.photos.photo.length === 10 && output.photos.total > 0).toBeTruthy();
+  it('should search empty names images corretly', (done: DoneFn) => {
+      service.search('', 10, 1).subscribe((output: FlickrOutput) => {
+        console.log(output)
+        expect(output.photos.photo.length === 0 && output.photos.total === 0).toBeTruthy();
         done();
-      });
-    }
+      })
   });
 });
--- src/app/services/flickr.service.ts
+++ src/app/services/flickr.service.ts
@@ -3,6 +3,7 @@ import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,9 +13,21 @@ export class FlickrService {
   constructor(private http: HttpClient) { }
 
   search(keyword: string, perPage: number, currPage: number) {
-    const url = 'https://www.flickr.com/services/rest/?method=flickr.photos.search&';
-    const params = `api_key=${environment.flickr.key}&text=${keyword}&format=json&nojsoncallback=1&per_page=${perPage}&page=${currPage}`;
+    if (keyword.length > 0) {
+      const url = 'https://www.flickr.com/services/rest/?method=flickr.photos.search&';
+      const params = `api_key=${environment.flickr.key}&text=${keyword}&format=json&nojsoncallback=1&per_page=${perPage}&page=${currPage}`;
 
-    return this.http.get<FlickrOutput>(url + params);
+      return this.http.get<FlickrOutput>(url + params);
+    } else {
+      const emptyOutput: FlickrOutput = {
+        photos: {
+          photo: [],
+          pages: 0,
+          perPage: 0,
+          total: 0
+        }
+      }
+      return of(emptyOutput);
+    }
   }
 }
